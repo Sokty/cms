dedfedc9d2651f9da649169b0eb3a4cb1243464e
Sokty <soktyeam@gmail.com>
2018-05-30 13:36:47 +0700

    Initial commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Http\Controllers;
+
+class PagesController extends Controller{
+
+	public function getIndex(){
+		return view('pages.welcome');
+		//return view('welcome'); if not in sub-folder
+	}
+
+	public function getAbout(){
+		return "<h1>About</h1>";
+	}
+
+	public function getContact(){
+		return view('pages.contact');
+	}
+
+
+
+}
+
+
+
+
+
+
+?>
